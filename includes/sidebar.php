0f7e6cc557b07980e90964375a12f2f326fbf6d3
Baguss-hub <2411500064@mahasiswa.atmaluhur.ac.id>
2025-11-22 21:04:04 +0700

    edit: sidebar.php

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -19,18 +19,22 @@
                         <a class="nav-link" href="layout-sidenav-light.html">Tambah Buku</a>
                     </nav>
                 </div>
-                <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapsePages" aria-expanded="false" aria-controls="collapsePages">
+
+                <!-- ini untuk menu kategori -->
+                <a class="nav-link <?php echo ($page == "daftar-kategori" || $page == "tambah-kategori" || $page == "ubah-kategori")? "active" : "collapsed"?>" href="#" data-bs-toggle="collapse" data-bs-target="#collapsePages" aria-expanded="false" aria-controls="collapsePages">
                     <div class="sb-nav-link-icon"><i class="fas fa-folder-open"></i></div>
                     Kategori
                     <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
                 </a>
-                <div class="collapse" id="collapsePages" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
+                <div class="collapse <?php echo ($page == "daftar-kategori" || $page == "tambah-kategori" || $page == "ubah-kategori")? "show" : ""?>" id="collapsePages" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
                     <nav class="sb-sidenav-menu-nested nav">
-                        <a class="nav-link" href="layout-static.html">Daftar Kategori</a>
-                        <a class="nav-link" href="layout-sidenav-light.html">Tambah Kategori</a>
+                        <a class="nav-link <?php echo ($page == "daftar-kategori")? 'active' : '';  ?>" href="index.php?hal=daftar-kategori">Daftar Kategori</a>
+                        <a class="nav-link <?php echo ($page == "tambah-kategori")? 'active' : '';  ?>" href="index.php?hal=tambah-kategori">Tambah Kategori</a>
                     </nav>
                 </div>
+                <!-- batas menu kategori -->
 
+                <!--  -->
                 <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#colapsAnggota" aria-expanded="false" aria-controls="colapsAnggota">
                     <div class="sb-nav-link-icon"><i class="fas fa-user"></i></div>
                     Anggota
